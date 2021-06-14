f2121edbd110e22f213ed3ee15a2262459bc2518
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-06-14 14:07:52 -0300

    Update Fornecedor.cpp

--- Fornecedor.cpp
+++ Fornecedor.cpp
@@ -8,7 +8,41 @@ Fornecedor::Fornecedor(string nome, string en, double tel, float valCred, float 
     this->valorCred = valCred;
     this->valorDiv = valDiv;
 }
+//metodos
+void Fornecedor::adicionaPessoa()
+{
+    cout << "adiciona pessoa ok" << endl;
 
+}
+
+void Fornecedor::atualizaPessoa()
+{
+    cout << "atualiza pessoa ok" << endl;
+
+}
+
+void Fornecedor::imprimeInfo()
+{
+    cout << nome << endl;
+
+}
+
+float Fornecedor::obterSaldo()
+{
+    float saldo;
+    saldo = (this->valorCred - this->valorDiv);
+
+    cout << "Saldo e: " << saldo << endl;
+
+    return 0;
+}
+
+void Fornecedor::listaTodos()
+{
+
+}
+
+//gets e sets
 float Fornecedor::getValorCred()
 {
     return valorCred;
@@ -29,12 +63,5 @@ void Fornecedor::setValorDiv(float valDiv)
     this->valorDiv = valDiv;
 }
 
-float Fornecedor::obterSaldo()
-{
-    float saldo;
-    saldo = (this->valorCred - this->valorDiv);
 
-    cout << saldo << endl;
 
-    return 0;
-}
