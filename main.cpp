2d49ef4034afd24181e0459608bf70477a54c9cf
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-06-14 14:06:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,20 @@
 
 int main()
 {
+    Pessoa p1("Fulano", "Rua Laurindo, 239, Porto Alegre RS", 51994958667);
+
+    p1.imprimeInfo();
+
+    p1.atualizaPessoa(Pessoa pessoa);
+
+    p1.imprimeInfo();
+/*
+    Fornecedor f1("Eduardo","Rua Carazinho, 546, Porto Alegre RS", 51993983502, 5000, 2345);
+
+    f1.obterSaldo();
+
+    f1.imprimeInfo();
+*/
 
  return 0;
 }
