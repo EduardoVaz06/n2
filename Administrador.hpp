d52a598dcc079d90df01fcd5f4b083ba011872e0
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-06-14 21:52:51 -0300

    Update Administrador.hpp

--- Administrador.hpp
+++ Administrador.hpp
@@ -9,16 +9,17 @@ class Administrador : public Empregado
 {
 public:
     Administrador(){};
-    Administrador(string n, string en, double tel, float salBase, float imp, int codSet, float ajuCust);
+    Administrador(string n, string en, int tel, float salBase, float imp, int codSet, float ajuCust);
 
     void setAjudaCusto(float ajuCust);
     float getAjudaCust();
 
     void calculaSalario();
 
+    void imprimeInfo();
+
 protected:
     float ajudaCusto;
 };
 
 #endif
-
