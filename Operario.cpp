766603db5dc5702b3f2a964866e7773d5e0544ed
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-06-14 22:04:31 -0300

    Update Operario.cpp

--- Operario.cpp
+++ Operario.cpp
@@ -2,7 +2,7 @@
 
 Operario::Operario(string n, string en, int tel, float salBase, float imp, int codSet, float valProd, float comiss)
 {
-    this->nome = nome;
+    this->nome = n;
     this->endereco = en;
     this->telefone = tel;
     this->salarioBase = salBase;
